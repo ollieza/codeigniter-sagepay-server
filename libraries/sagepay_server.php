3d75c24f4cb3d1e87e1671f4d51df031f10b574d
ollieza <288799+ollieza@users.noreply.github.com>
2022-04-04 17:44:50 +0200

    Update sagepay_server.php

--- libraries/sagepay_server.php
+++ libraries/sagepay_server.php
@@ -8,10 +8,7 @@
  * http://www.sagepay.com/products_services/sage_pay_go/integration/server
  * 
  * @package	  sagepay_server
- * @author    Ollie Rattue, Too many tabs <orattue[at]toomanytabs.com>
- * @copyright Copyright (c) 2011, Ollie Rattue
  * @license   http://www.opensource.org/licenses/mit-license.php
- * @link      http://github.com/ollierattue/codeigniter-sagepay-server
  */
 
 class sagepay_server
